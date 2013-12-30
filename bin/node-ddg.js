726d1b75619d2c10f2c0d4d3ac517b2653f24cd0
Luke Wendling <luke@lukewendling.com>
2013-12-30 16:13:49 -0600

    jshint-ed for node in strict mode

--- bin/node-ddg.js
+++ bin/node-ddg.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
-var DDG = require('../index').DDG;
-var argv = require('optimist')
-  .usage('Query DuckDuckGo Instant Answer API\nPass exact options as shown: https://duckduckgo.com/api\nUsage: $0 <searchterm> --t appname --pretty 1 --skip_disambig 1')
-  .demand('t')
-  .describe('t', 'Tell DDG the name of your app')
-  .argv;
+'use strict';
 
-ddg = new DDG(argv.t);
+var DDG = require('../index').DDG,
+  argv = require('optimist')
+    .usage('Query DuckDuckGo Instant Answer API\nPass exact options as shown: https://duckduckgo.com/api\nUsage: $0 <searchterm> --t appname --pretty 1 --skip_disambig 1')
+    .demand('t')
+    .describe('t', 'Tell DDG the name of your app')
+    .argv,
+  ddg = new DDG(argv.t);
+
 ddg.instantAnswer(argv._[0], argv);
--- index.js
+++ index.js
@@ -1,1 +1,3 @@
+'use strict';
+
 exports.DDG = require('./lib/ddg');
